70550c06e8590436eab57ce915aa0d3702071c0f
fx109138 <1091381572@qq.com>
2019-06-08 20:54:51 +0800

    change company

--- bin/card.js
+++ bin/card.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
 
-// Pull in our modules
+// 👆 Used to tell Node.js that this is a CLI tool Pull in our modules
 const chalk = require('chalk')
 const boxen = require('boxen')
 
@@ -14,28 +13,52 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.magenta.bold('隋堤倦客'),
-  handle: chalk.yellow.bold('fengxu'),
+  name: chalk
+    .magenta
+    .bold('隋堤倦客'),
+  handle: chalk
+    .yellow
+    .bold('fengxu'),
   email: chalk.white('fengxuneau@foxmail.com'),
   tel: chalk.white('13009715105'),
   wechatQQ: chalk.white('1091381572'),
-  work: chalk.white('Frontend Engineer at Meituan-Dianping'),
+  work: chalk.white('Frontend Engineer at ByteDance'),
   location: chalk.white('Beijing, China'),
   github: chalk.cyan('https://github.com/fx109138'),
   juejin: chalk.cyan('https://juejin.im/user/585c72fb8d6d810065d820b8'),
   maimai: chalk.cyan('https://maimai.cn/contact/detail/194733023'),
   web: chalk.cyan('http://fengxu.ink'),
   npx: chalk.white('npx fengxu'),
-  labelEmail: chalk.red.bold('     Email:'),
-  labelTel: chalk.red.bold('       TEL:'),
-  labelWechatQQ: chalk.red.bold(' QQ/Wechat:'),
-  labelWork: chalk.green.bold('      Work:'),
-  labelLocation: chalk.green.bold('  Location:'),
-  labelGitHub: chalk.blue.bold('    GitHub:'),
-  labelJuejin: chalk.blue.bold('    Juejin:'),
-  labelMaimai: chalk.blue.bold('    Maimai:'),
-  labelWeb: chalk.blue.bold('       Web:'),
-  labelCard: chalk.cyan.bold('      Card:')
+  labelEmail: chalk
+    .red
+    .bold('     Email:'),
+  labelTel: chalk
+    .red
+    .bold('       TEL:'),
+  labelWechatQQ: chalk
+    .red
+    .bold(' QQ/Wechat:'),
+  labelWork: chalk
+    .green
+    .bold('      Work:'),
+  labelLocation: chalk
+    .green
+    .bold('  Location:'),
+  labelGitHub: chalk
+    .blue
+    .bold('    GitHub:'),
+  labelJuejin: chalk
+    .blue
+    .bold('    Juejin:'),
+  labelMaimai: chalk
+    .blue
+    .bold('    Maimai:'),
+  labelWeb: chalk
+    .blue
+    .bold('       Web:'),
+  labelCard: chalk
+    .cyan
+    .bold('      Card:')
 }
 
 // Actual strings we're going to output
@@ -52,18 +75,8 @@ const maimaiing = `${data.labelMaimai}  ${data.maimai}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
-// Put all our output together into a single variable so we can use boxen effectively
-const output = 
-  heading + newline + newline +
-  emailing + newline +
-  teling + newline +
-  wechatQQing + newline + newline +
-  working + newline +
-  locating + newline + newline +
-  githubing + newline +
-  juejining + newline +
-  maimaiing + newline +
-  webing + newline + newline +
-  carding
+// Put all our output together into a single variable so we can use boxen
+// effectively
+const output = heading + newline + newline + emailing + newline + teling + newline + wechatQQing + newline + newline + working + newline + locating + newline + newline + githubing + newline + juejining + newline + maimaiing + newline + webing + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
